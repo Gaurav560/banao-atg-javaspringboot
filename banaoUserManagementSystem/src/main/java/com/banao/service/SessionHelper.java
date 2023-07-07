38f198d8bd1cf6ec89e0b829c6c4c61b504c1d84
GAURAV SHARMA <gsharma1154@gmail.com>
2023-07-07 13:27:03 +0530

    solved removeattribute session error in thymeleaf .it was a version error as session.removeAttribute is deprecated

---
+++ banaoUserManagementSystem/src/main/java/com/banao/service/SessionHelper.java
@@ -0,0 +1,28 @@
+package com.banao.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import jakarta.servlet.http.HttpSession;
+
+@Component
+public class SessionHelper {
+
+	public void removeMessageFromSession() {
+
+		try {
+
+			HttpSession session = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest()
+					.getSession();
+			
+			session.removeAttribute("sucMsg");
+
+		}
+
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
